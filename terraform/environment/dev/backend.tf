8ff118f0546221fdd9c5e4cde5d44f824f9d6e3a
Leelavathy <vleelavathy88@gmail.com>
2026-02-07 21:30:36 -0600

    terraform update workflow

--- terraform/environment/dev/backend.tf
+++ terraform/environment/dev/backend.tf
@@ -4,6 +4,6 @@ terraform {
     resource_group_name  = "le-app-westus2-stg-rg"
     storage_account_name = "westus2stgstorageact"
     container_name       = "tfstate"
-    key                  = "stg/dev_env.tfstate"
+    key                  = "dev/dev_env.tfstate"
   }
 }
