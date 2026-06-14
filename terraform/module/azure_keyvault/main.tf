7702ab85fadd4fd096e0ae5b34663b60e9031258
Leelavathy <vleelavathy88@gmail.com>
2026-06-14 14:52:49 -0500

    changes

--- terraform/module/azure_keyvault/main.tf
+++ terraform/module/azure_keyvault/main.tf
@@ -1,14 +1,9 @@
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group
-  location = var.location
-}
-
 resource "azurerm_key_vault" "keyvault" {
   name                        = "${var.appName}-${var.environment}-keyvault"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
+  location                    = var.location
+  resource_group_name         = var.resource_group
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
